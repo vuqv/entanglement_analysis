564694f5b7f5e435d16eef30e4b7fbefa972ad05
Quyen Vu <vuqv.phys@gmail.com>
2021-08-14 20:57:29 +0200

    reorgarnize single_frame.py code

--- single_frame.py
+++ single_frame.py
@@ -1,7 +1,7 @@
 """
 The length of loop and open segment >=10 (i1-i2 and j1-j2>=10)
 I don't know why authors used this criteria but just use this.
-usage: python ent_calculation.py PDB_ID
+usage: python ent_calculation.py PDB_ID.pdb
 """
 import itertools
 import sys
@@ -9,11 +9,44 @@ import time
 
 import MDAnalysis as mda
 import numpy as np
-from numba import njit, prange
+from numba import njit
 
 pdb = sys.argv[1]
-len_seg = 10 #predefine minimum segment length, |j-i| >=len_seg
+len_seg = 10  # predefine minimum segment length, |j-i| >=len_seg
 begin_time = time.time()
+
+# define function
+
+"""
+note on numba extension:
+@njit= @jit(nopython=True) this will boost the performance dramatically
+fastmath=True: in this function (example) the reduction can be vectorized as floating point reassociation is permitted.
+argument parallel=True used with prange will be consider when working with trajectories where prange use to parallel for frame, in current suituation, it is slower
+since split threads and then waiting for result, joint the results...
+@njit(fastmath=True, parallel=True)
+"""
+
+
+@njit(fastmath=True)
+def cal_gc_ij(_i1, _i2, _j1, _j2):
+    """
+    This function calculates the double integral (4)
+    Baiesi, M., Orlandini, E., Seno, F., & Trovato, A. (2017).
+    Exploring the correlation between the folding rates of proteins and the entanglement of their native states.
+    Journal of Physics A: Mathematical and Theoretical, 50(50). https://doi.org/10.1088/1751-8121/aa97e7
+    :param _i1: (int) the first index of the loop
+    :param _i2: (int) the second index of the loop
+    :param _j1: (int) the first index of open segment
+    :param _j2: (int) the second index of open segment
+    :return: (float) |G|_{ij}
+    """
+    _gc_ij = 0.0
+    for i in range(_i1, _i2):
+        for j in range(_j1, _j2):
+            _gc_ij += np.dot(R_diff_3d[i, j, :] / np.linalg.norm(R_diff_3d[i, j, :]) ** 3, dR_cross_3d[i, j, :])
+    _gc_ij = _gc_ij / (4 * np.pi)
+    return _gc_ij, _i1, _i2, _j1, _j2
+
 
 u = mda.Universe(pdb, pdb)
 ca_atoms = u.select_atoms("name CA")
@@ -47,37 +80,6 @@ pair_array = np.asarray(list(itertools.product(raw_positions, raw_positions)))
 Distance_diff = pair_array[:, 0, :] - pair_array[:, 1, :]
 Distance_pair = np.linalg.norm(Distance_diff, axis=1).reshape(nAtoms, nAtoms)
 
-
-
-"""
-note on numba extension:
-@njit= @jit(nopython=True) this will boost the performance dramatically
-fastmath=True: in this function (example) the reduction can be vectorized as floating point reassociation is permitted.
-argument parallel=True used with prange will be consider when working with trajectories where prange use to parallel for frame, in current suituation, it is slower
-since split threads and then waiting for result, joint the results...
-@njit(fastmath=True, parallel=True)
-"""
-@njit(fastmath=True)
-def cal_gc_ij(_i1, _i2, _j1, _j2):
-    """
-    This function calculates the double integral (4)
-    Baiesi, M., Orlandini, E., Seno, F., & Trovato, A. (2017).
-    Exploring the correlation between the folding rates of proteins and the entanglement of their native states.
-    Journal of Physics A: Mathematical and Theoretical, 50(50). https://doi.org/10.1088/1751-8121/aa97e7
-    :param _i1: (int) the first index of the loop
-    :param _i2: (int) the second index of the loop
-    :param _j1: (int) the first index of open segment
-    :param _j2: (int) the second index of open segment
-    :return: (float) |G|_{ij}
-    """
-    _gc_ij = 0.0
-    for i in range(_i1, _i2):
-        for j in range(_j1, _j2):
-            _gc_ij += np.dot(R_diff_3d[i, j, :] / np.linalg.norm(R_diff_3d[i, j, :]) ** 3, dR_cross_3d[i, j, :])
-    _gc_ij = _gc_ij / (4 * np.pi)
-    return _gc_ij, _i1, _i2, _j1, _j2
-
-
 """
  main loop, looking for all contact pairs (i_ loop) and 
  for each contact, looking for all segment possibility (j loop) and calculate Gij for that pair.
@@ -97,4 +99,4 @@ for (i1, i2) in [(i1, i2) for i1 in range(N - len_seg + 1) for i2 in range(i1 + 
 
 end_time = time.time()
 total_run_time = end_time - begin_time
-print(f'Total execution time: {total_run_time / 60.0:.3f} mins') 
+print(f'Total execution time: {total_run_time / 60.0:.3f} mins')
