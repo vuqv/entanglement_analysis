6820ecbb54d1d54f9796f3a380a61d91bd76b60c
Quyen Vu <vuqv.phys@gmail.com>
2022-06-11 15:33:09 +0200

    edit pre_compile.jl to create image

--- precompile_module.jl
+++ precompile_module.jl
@@ -1,6 +1,7 @@
 using PackageCompiler
 
-create_sysimage([:MDToolbox, :Distances, :ArgParse], sysimage_path="gauss_image.so", precompile_execution_file="precompile_plots.jl")
+# create_sysimage([:MDToolbox, :Distances, :ArgParse], sysimage_path="gauss_image.so", precompile_execution_file="precompile_plots.jl")
+create_sysimage([:MDToolbox, :Distances, :ArgParse], sysimage_path="gauss_image.so")
 
 # run code with image:
 # julia -J gauss_image.so --trace-compile=stderr gauss_linking.jl -f XXX -p YYY
